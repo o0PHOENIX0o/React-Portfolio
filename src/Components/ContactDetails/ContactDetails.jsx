bde10fec2de1b6d44d715fcdf3ab90e01dbef6c2
o0PHOENIX0o <pheonixyt5@gmail.com>
2024-08-08 01:39:51 +0530

    contact page

--- src/Components/ContactDetails/ContactDetails.jsx
+++ src/Components/ContactDetails/ContactDetails.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from './ContactDetails.module.css';
 import profileLogo from '../../assets/profile.png';
+import GitHubLogo from '../../assets/logos/github.svg'
+import LinkedinLogo from '../../assets/logos/linkedin.svg'
+import MailLogo from '../../assets/logos/mail.svg'
 
 function ContactDetails() {
     return (
@@ -11,13 +14,17 @@ function ContactDetails() {
                     <div className={`${styles.title} ${styles.card}`}>
                         <h2>Contact Me</h2>
                         <hr />
-                        <p></p>
                     </div>
 
                     <div className={`${styles.mail} ${styles.card}`}>
                         <h2>Mail me</h2>
                         <hr />
-                        <p></p>
+                        <div>
+                            <div className={styles.mailContainer}>
+                                <img src={MailLogo} alt="MailLogo" />
+                                <p>ujjwallehri33 @gmail.com</p>
+                            </div>
+                        </div>
                     </div>
 
                     <div className={`${styles.profile} ${styles.card}`}>
@@ -28,15 +35,33 @@ function ContactDetails() {
                     </div>
 
                     <div className={`${styles.linkedin} ${styles.card}`}>
-                        <h2>Connect on LinkedIn</h2>
+                        <h2>LinkedIn Profile</h2>
                         <hr />
-                        <p></p>
+                        <div>
+                            <div className={styles.linkedinContainer}>
+                                <div className={styles.linkedinLogo}>
+                                    <img src={LinkedinLogo} alt="linkedinLogo" />
+                                </div>
+                                <p>Connect with me on LinkedIn</p>
+                            </div>
+                        </div>
+                        
+
                     </div>
 
                     <div className={`${styles.github} ${styles.card}`}>
-                        <h2>GitHub</h2>
-                        <p></p>
+                        <h2>GitHub Profile</h2>
+                        <hr />
+                        <div>
+                            <div className={styles.githubLogoContainer}>
+                                <div className={styles.githubLogo}>
+                                    <img src={GitHubLogo} alt="GitHubLogo" />
+                                </div>
+                                <p>Explore my projects.</p>
+                            </div>
+                        </div>
                     </div>
+
 
                 </div>
             </div>
