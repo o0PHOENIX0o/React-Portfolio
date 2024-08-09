64ef6a57f4ae5236979272412b99aa4118747812
o0PHOENIX0o <pheonixyt5@gmail.com>
2024-08-09 17:35:47 +0530

    contact page hover eeffect

--- src/Components/ContactDetails/ContactDetails.jsx
+++ src/Components/ContactDetails/ContactDetails.jsx
@@ -13,52 +13,49 @@ function ContactDetails() {
             <div className={styles.mainContainer}>
                 <div className={styles.container}>
 
-                    <div className={`${styles.title} ${styles.card}`}>
+                    <div className={`${styles.card} ${styles.contactTitle}`}>
                         <h2>Contact Me</h2>
                         <hr />
                     </div>
 
-                    <div onClick={() => window.location.href = 'mailto:ujjwallehri33@gmail.com'} className={`${styles.mail} ${styles.card}`}>
+                    <div onClick={() => window.location.href = 'mailto:ujjwallehri33@gmail.com'} className={`${styles.mail} ${styles.card} ${styles.mailCard}`}>
                         <h2>Mail me</h2>
                         <hr />
-                        <div>
-                            <div className={`${styles.mailContainer} ${styles.hoverdiv }`}>
+                        <div className={styles.mailContent}>
+                            <div className={`${styles.mailContainer} ${styles.hoverdiv}`}>
                                 <div className={styles.mailProfile}>
                                     <img src={profilePic} alt="MailLogo" /> 
                                 </div>
-                                <p>ujjwallehri33@gmail.com</p>
+                                <p>ujjwallehri33 @gmail.com</p>
                             </div>
                         </div>
                     </div>
 
-                    <div className={`${styles.profile} ${styles.card}`}>
+                    <div className={`${styles.card} ${styles.profileCard}`}>
                         <h2>Ujjwal Lehri</h2>
-                        <div className={styles.profileImg}>
+                        <div className={styles.profileImgContainer}>
                             <img src={profileLogo} alt="Profile" />
                         </div>
                     </div>
 
-                    <div className={`${styles.linkedin} ${styles.card}`}>
+                    <div className={`${styles.card} ${styles.linkedinCard}`}>
                         <h2>LinkedIn Profile</h2>
                         <hr />
-                        <div>
-                            <div onClick={() => window.open('https://www.linkedin.com/in/ujjwal-lehri/', '_blank')} className={`${styles.linkedinContainer} ${styles.hoverdiv }`}>
-                            
+                        <div className={styles.linkedinContent}>
+                            <div onClick={() => window.open('https://www.linkedin.com/in/ujjwal-lehri/', '_blank')} className={`${styles.linkedinContainer} ${styles.hoverdiv}`}>
                                 <div className={styles.linkedinLogo}>
                                     <img src={profilePic} alt="linkedinLogo" />
                                 </div>
                                 <p>Connect with me on LinkedIn</p>
                             </div>
                         </div>
-                        
-
                     </div>
 
-                    <div className={`${styles.github} ${styles.card}`}>
+                    <div className={`${styles.card} ${styles.githubCard}`}>
                         <h2>GitHub Profile</h2>
                         <hr />
-                        <div>
-                            <div onClick={() => window.open('https://github.com/o0PHOENIX0o', '_blank')} className={`${styles.githubLogoContainer} ${styles.hoverdiv }`}> 
+                        <div className={styles.githubContent}>
+                            <div onClick={() => window.open('https://github.com/o0PHOENIX0o', '_blank')} className={`${styles.githubLogoContainer} ${styles.hoverdiv}`}>
                                 <div className={styles.githubLogo}>
                                     <img src={gitProile} alt="GitHubLogo" />
                                 </div>
@@ -67,9 +64,10 @@ function ContactDetails() {
                         </div>
                     </div>
 
-
                 </div>
             </div>
+
+
         </>
     );
 }
