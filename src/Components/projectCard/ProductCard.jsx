458f4ab5a0ca3e973a99be191e43497b3252114a
o0PHOENIX0o <pheonixyt5@gmail.com>
2024-08-09 21:18:01 +0530

    projects section layout

--- src/Components/projectCard/ProductCard.jsx
+++ src/Components/projectCard/ProductCard.jsx
@@ -1,15 +1,69 @@
-import react from "react";
+import React from "react";
+import styles from './ProductCard.module.css';
 
-function Project_card(project){
-    return(
+function ProjectCard(props) { // Renamed to match convention
+
+    const handleIconClick = (url, message) => {
+        if (message) {
+            alert(message);
+        } else {
+            window.open(url, '_blank');
+        }
+    };
+
+    const projects = props.projects;
+
+    return (
         <>
-            <div className="project-card">
-                <div className="project-image">
-                    <img src="" alt="" />
-                </div>
-            </div>
+            {projects.map((project, index) => (
+                <li
+                    key={index}
+                    className={`${styles.projectItem} ${styles.active}`} // Use CSS module class
+                    data-category="web development"
+                >
+                    <div className={styles.projectCard}> {/* Use CSS module class */}
+                        <figure className={styles.projectImg}> {/* Use CSS module class */}
+                            <div
+                                className={styles.projectItemIconBox} // Use CSS module class
+                                style={{ left: '35%' }}
+                                onClick={() =>
+                                    handleIconClick(project.demoUrl, project.alertMessage)
+                                }
+                            >
+                                <ion-icon
+                                    name="eye-outline"
+                                    role="img"
+                                    aria-label="eye outline"
+                                ></ion-icon>
+                            </div>
+                            <div
+                                className={styles.projectItemIconBox} // Use CSS module class
+                                style={{ left: '65%' }}
+                                onClick={() =>
+                                    handleIconClick(project.githubUrl, project.alertMessage)
+                                }
+                            >
+                                <ion-icon
+                                    name="logo-github"
+                                    role="img"
+                                    aria-label="logo github"
+                                ></ion-icon>
+                            </div>
+
+                            <img
+                                src={project.img}
+                                alt={project.title}
+                                loading="lazy"
+                            />
+                        </figure>
+
+                        <h3 className={styles.projectTitle}>{project.title}</h3> {/* Use CSS module class */}
+                        <p className={styles.projectCategory}>{project.subTitle}</p> {/* Use CSS module class */}
+                    </div>
+                </li>
+            ))}
         </>
-    )
+    );
 }
 
-export default Project_card
+export default ProjectCard;
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,16 +1,32 @@
-import react from "react";
-import '../assets/css/index.css'
-import Header from '../Components/Header/Header.jsx'
+import React from "react";
+import Header from '../Components/Header/Header.jsx';
+import Project_card from "../Components/projectCard/ProductCard.jsx";
+import EcomProject from '../assets/projects/Ecom-project.png'
+import '../assets/css/index.css';
 
-function Projects(project){
-    return(
+
+const projects = [
+    {
+        title: "E-Commerce Website",
+        subTitle: "Web Development",
+        img: EcomProject,
+        demoUrl: "https://ecommerce-electronics-website.vercel.app",
+        githubUrl: "https://github.com/o0PHOENIX0o/Ecommerce-Website",
+        // alertMessage: "You are viewing the E-Commerce demo!",
+    }
+];
+
+
+function Projects() {
+    
+    return (
         <>
             <Header />
-            
-            
-
+            <ul className="projectList">
+                <Project_card projects={projects} />
+            </ul>
         </>
-    )
+    );
 }
 
-export default Projects
+export default Projects;
